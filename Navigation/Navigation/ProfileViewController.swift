a92e9edccb63ab95eaa79219dbfdfff203f6d90d
Xoxol1488 <kirillponomarenko322@gmail.com>
2022-05-24 20:20:46 +0300

    случайно залил пустой файл

--- Navigation/Navigation/ProfileViewController.swift
+++ Navigation/Navigation/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Navigation
 //
 //  Created by Кирилл Пономаренко on 19.05.2022.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ProfilViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Профиль"
+        view.backgroundColor = .systemGray5
     }
-
-
+ 
 }
 
