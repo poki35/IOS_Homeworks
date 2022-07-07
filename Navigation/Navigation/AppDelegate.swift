eeca23c2a23724bd31be91552b21a7da4d2126d2
poki35 <kirillponomarenko322@gmail.com>
2022-07-07 16:20:15 +0300

    homework done

--- Navigation/Navigation/AppDelegate.swift
+++ Navigation/Navigation/AppDelegate.swift
@@ -11,12 +11,33 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-      func application(_ application: UIApplication, didFinishLaunchingWithOptions           launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-           window = UIWindow(frame: UIScreen.main.bounds)
-           window?.rootViewController = MainTabBarController()
-           window?.makeKeyAndVisible()
-      return true
-    }
+        
+        var tabBarController = UITabBarController()
+        let feedViewController = FeedViewController()
+        let logInViewController = LogInViewController()
+
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+            let feed = UINavigationController(rootViewController: feedViewController)
+            
+            let profile = UINavigationController(rootViewController: logInViewController)
+            profile.navigationBar.isHidden = true
+            
+            tabBarController.viewControllers = [feed, profile]
+            tabBarController.tabBar.backgroundColor = .systemGray4
+            tabBarController.tabBar.layer.borderWidth = 0.5
+            tabBarController.tabBar.layer.borderColor = UIColor.black.cgColor
+            
+            feed.tabBarItem.title = "Feed"
+            feed.tabBarItem.image = UIImage(systemName: "books.vertical.fill")
+            
+            profile.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName:"person.fill"), tag: 1)
+
+            window = UIWindow()
+            window?.rootViewController = tabBarController
+            window?.makeKeyAndVisible()
+            
+            return true
+        }
     
 }
---
+++ Navigation/Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,90 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Кирилл Пономаренко on 07.07.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    let scrollView: UIScrollView = {
+            let scrollView = UIScrollView()
+            scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+            return scrollView
+        }()
+        
+        let contentView: LogInView = {
+            let contentView = LogInView()
+            contentView.translatesAutoresizingMaskIntoConstraints = false
+            contentView.layout()
+
+            return contentView
+        }()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            view.backgroundColor = .white
+            layout()
+            
+            contentView.logInButton.addTarget(self, action: #selector(pushInProfile), for: .touchUpInside)
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            let nc = NotificationCenter.default
+            nc.addObserver(self, selector: #selector(kbdShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            nc.addObserver(self, selector: #selector(kbdHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+        
+        override func viewDidDisappear(_ animated: Bool) {
+            super.viewDidDisappear(animated)
+            
+            let nc = NotificationCenter.default
+            nc.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+            nc.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
+
+        @objc private func kbdShow(notification: NSNotification) {
+            if let kbdSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                scrollView.contentInset.bottom = kbdSize.height
+                scrollView.verticalScrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbdSize.height, right: 0)
+            }
+        }
+        
+        @objc private func kbdHide(notification: NSNotification) {
+            scrollView.contentInset.bottom = .zero
+            scrollView.verticalScrollIndicatorInsets = .zero
+        }
+
+        
+        private func layout() {
+            
+            view.addSubview(scrollView)
+            scrollView.addSubview(contentView)
+            
+            NSLayoutConstraint.activate([scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+                                         scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+                                         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                                         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                                         
+                                         contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+                                         contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+                                         contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+                                         contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                                         contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)])
+            
+        }
+        
+        @objc func pushInProfile() {
+            
+            let profileViewController = ProfileViewController()
+            navigationController?.pushViewController(profileViewController, animated: true)
+            
+        }
+    
+}
